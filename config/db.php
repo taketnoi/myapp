549b0de2c5760fbb6433d818fee16fb77db21200
taketnoi <nguyentienhung@msn.com>
2014-11-27 16:07:27 +0700

    Add config/db.php

---
+++ config/db.php
@@ -0,0 +1,7 @@
+<?php
+// database connection details
+$hostname_DB = "<%= @database['host'] %>";
+$username_DB = "<%= @database['user'] %>";
+$password_DB = "<%= @database['password'] %>";
+$database_DB = "<%= @database['schema'] %>";
+?>
